a2d5d4830ababc6bbb7e7e904b8e658faac5530d
Fuad1711 <ff.fuad01@gmail.com>
2026-08-06 07:50:25 +0600

    Fix missing Color import in BattleScreen from upstream merge

--- core/src/main/java/com/cardgame/screens/BattleScreen.java
+++ core/src/main/java/com/cardgame/screens/BattleScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
